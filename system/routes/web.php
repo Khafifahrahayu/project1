b3a3a6b2512043c6789d04b62a57da970b839abd
Khafifahrahayu <71141098+Khafifahrahayu@users.noreply.github.com>
2020-10-27 16:09:50 +0700

    Initial commit

---
+++ system/routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.index');
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/cart', function () {
+    return view('pages.cart');
+});
+
+Route::get('/checkout', function () {
+    return view('pages.checkout');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+
+Route::get('/index', function () {
+    return view('pages.index');
+});
+
+
+Route::get('/shop-single', function () {
+    return view('pages.shop-single');
+});
+
+
+Route::get('/shop', function () {
+    return view('pages.shop');
+});
+
+
+Route::get('/thankyou', function () {
+    return view('pages.thankyou');
+}); 
+
+Route::get('/login', function () {
+    return view('pages.login');
+});
+
